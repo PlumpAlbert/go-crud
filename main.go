12d0b6170d262825d2053e68e93979b6c67ab0f2
Plump Albert <plumpalbert@gmail.com>
2025-04-17 14:09:20 +0300

    connect to db

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+var db *sql.DB
+
+func initDB() {
+	var err error
+
+	db, err = sql.Open("mysql", "root:root@(127.0.0.1)/testdb?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check DB connection
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	initDB()
+	defer db.Close()
+
 	gRouter := mux.NewRouter()
 
 	gRouter.HandleFunc("/", HomeHandler)
